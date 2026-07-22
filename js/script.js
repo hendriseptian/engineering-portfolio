510849167014041ebc1a6e6b6418a68fc8cbccdc
hendriseptian <hendriseptian25@gmail.com>
2026-07-22 14:51:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
+const images = document.querySelectorAll(".gallery img");
 
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImage = document.getElementById("lightbox-img");
+
+const close = document.querySelector(".close");
+
+images.forEach(img=>{
+
+    img.addEventListener("click",function(){
+
+        lightbox.style.display="flex";
+
+        lightboxImage.src=this.src;
+
+    });
+
+});
+
+close.addEventListener("click",function(){
+
+    lightbox.style.display="none";
+
+});
+
+lightbox.addEventListener("click",function(e){
+
+    if(e.target!==lightboxImage){
+
+        lightbox.style.display="none";
+
+    }
+
+});
