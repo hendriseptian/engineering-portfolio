931c2e38745af46d9733079db5af176bbb08536f
hendriseptian <hendriseptian25@gmail.com>
2026-07-22 16:29:23 +0700

    Update script.js
    update js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,35 @@ const projects = [
 title:"Downspot Layout & Detail",
 folder:"downspot-layout-detail",
 images:[
-"01.jpg",
-"02.jpg",
-"03.jpg"
+"downspot layout & detail-1.jpg",
+"downspot layout & detail-2.jpg",
+"downspot layout & detail-3.jpg"
+]
+},
+
+{
+title:"General Arrangement Pressure Vessel",
+folder:"general-arrangement-pressure-vessel",
+images:[
+"PV 01A.jpg"
+]
+},
+
+{
+title:"General Arrangement Steel Building",
+folder:"general-arrangement-steel-building",
+images:[
+"general arrangement-1.jpg",
+"general arrangement-2.jpg",
+"general arrangement-3.jpg"
+]
+},
+
+{
+title:"General Arrangement Storage Tank",
+folder:"general-arrangement-storage-tank",
+images:[
+"TANK-001.jpg"
 ]
 },
 
@@ -14,9 +40,55 @@ images:[
 title:"Pipeline Schematic",
 folder:"pipeline-schematic",
 images:[
-"01.jpg",
-"02.jpg"
+"Pipeline Schematic Drawing.jpg"
+]
+},
+
+{
+title:"Piping & Instrumentation Diagram",
+folder:"piping-instrumentation-diagram",
+images:[
+"P&ID.jpg"
 ]
 }
 
 ];
+
+const container = document.getElementById("portfolio");
+const menu = document.getElementById("menu");
+
+projects.forEach(project => {
+
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="#${project.folder}">${project.title}</a>`;
+    menu.appendChild(li);
+
+    const section = document.createElement("section");
+    section.className = "project";
+    section.id = project.folder;
+
+    section.innerHTML = `
+        <h2>${project.title}</h2>
+        <p>${project.images.length} Drawing(s)</p>
+        <div class="gallery"></div>
+    `;
+
+    const gallery = section.querySelector(".gallery");
+
+    project.images.forEach(file => {
+
+        const img = document.createElement("img");
+
+        img.src = `portofolio/${project.folder}/${file}`;
+
+        img.loading = "lazy";
+
+        img.alt = project.title;
+
+        gallery.appendChild(img);
+
+    });
+
+    container.appendChild(section);
+
+});
